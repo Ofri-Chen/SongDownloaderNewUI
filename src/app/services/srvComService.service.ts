3f0dca57d2c854fdb3f9d1c7fbaf1350131f0b47
Thankjnvv <ofrichen11@gmail.com>
2019-06-15 15:33:37 +0300

    improved server SrvComService

--- src/app/services/srvComService.service.ts
+++ src/app/services/srvComService.service.ts
@@ -1,86 +1,53 @@
 import { Injectable, Optional } from '@angular/core';
-import { Http, Request } from '@angular/http';
+import { Http, Request, Response } from '@angular/http';
 import { DownloadTracksRoute } from '../Types/routes/downloadTracks';
-import { String } from "../Common/Extensions/stringExtensions";
+import { map } from 'rxjs/operators';
 
 import { Route } from '../Types/routes/route';
 import { ServerArtist } from '../Types/server-artist';
 import { TopTracksRoute } from '../Types/routes/topTracks';
 import { GetVideoIdRoute } from '../Types/routes/getVideoId';
 import { Artist } from '../Types/artist';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SrvComService {
-    constructor( @Optional() private _http: Http) { }
+    constructor(private _http: Http) { }
 
-    private _serverUrl: string = 'http://localhost:5555';
+    private _serverUrl = 'http://localhost:5555';
 
-    public getTopTracks(artistName: string, numOfTracks: number): Promise<ServerArtist> {
-        return new Promise((resolve, reject) => {
-            if (!this.isTopTracksParamsValid) {
-                reject("bad parameters");
-            }
-            else {
-                this.buildRequestUrl(new TopTracksRoute(artistName, numOfTracks))
-                    .then((requestUrl) => {
-                        this._http.get(requestUrl).subscribe(value => resolve(JSON.parse(value["_body"])));
-                    });
-            }
-        });
+    public async getTopTracks(artistName: string, numOfTracks: number): Promise<ServerArtist> {
+        if (!this.isTopTracksParamsValid) {
+            throw new Error('bad parameters');
+        }
+        const requestUri = await this.buildRequestUri(new TopTracksRoute(artistName, numOfTracks));
+        return this.parseResponse<ServerArtist>(this._http.get(requestUri));
     }
 
-    public getVideoId(artistName: string, trackName: string,
-        withLyrics: boolean, resultNum: number = 0): Promise<string> {
-        
-        return new Promise((resolve, reject) => {
-            this.buildRequestUrl(new GetVideoIdRoute(artistName, trackName, withLyrics, resultNum))
-                .then((requestUrl) => {
-                    this._http.get(requestUrl).subscribe((res) => {
-                        resolve(res["_body"]);
-                    })
-                }).catch(err => reject(err));
-        });
+    public getVideoId(artistName: string, trackName: string, withLyrics: boolean, resultNum: number = 0): Promise<string> {
+        const requestUri = this.buildRequestUri(new GetVideoIdRoute(artistName, trackName, withLyrics, resultNum));
+        return this.parseResponse(this._http.get(requestUri));
     }
 
     public downloadTracks(artist: Artist): Promise<Artist> {
-        // let serverArtist: ServerArtist = {
-        //     name: artist.name,
-        //     tracks: artist.tracks.map(track => track.name),
-        //     withLyrics: artist.withLyrics,
-        // }
-        return new Promise((resolve, reject) => {
-            this.buildRequestUrl(new DownloadTracksRoute())
-                .then((requestUrl) => {
-                    this._http.post(requestUrl, artist).subscribe((val) => {
-                        console.log(val);
-                        resolve(artist);
-                    });
-                })
-        });
+        const requestUri = this.buildRequestUri(new DownloadTracksRoute());
+        return this.parseResponse(this._http.post(requestUri, artist));
+    }
+
+    private parseResponse<T>(response: Observable<Response>): Promise<T> {
+        return response.pipe(map(value => JSON.parse(value['_body']))).toPromise()
     }
 
     private isTopTracksParamsValid(artistName: string, numOfTracks: number): boolean {
-        return artistName != "" && numOfTracks > 0;
+        return artistName !== '' && numOfTracks > 0;
     }
 
-    private buildRequestUrl(route: Route): Promise<string> {
-        return new Promise((resolve, reject) => {
-            if (route.params != null) {
-                this.buildQueryParams(route.params).then((queryParams) => {
-                    resolve(`${this._serverUrl}/${route.url}?${queryParams}`);
-                });
-            }
-            else {
-                resolve(`${this._serverUrl}/${route.url}`);
-            }
-        });
+    private buildRequestUri(route: Route): string {
+        return `${this._serverUrl}/${route.url}?${this.buildQueryParams(route.params)}`;
     }
 
-    private buildQueryParams(params: any): Promise<string> {
-        return new Promise((resolve, reject) => {
-            let queryParams = Object.keys(params)
-                .map((key) => `${params[key].name}=${params[key].value}`).join('&');
-            resolve(`${queryParams}`);
-        });
+    private buildQueryParams(params: any): string {
+        return Object.keys(params || {})
+            .map((key) => `${params[key].name}=${params[key].value}`).join('&');
     }
 }
